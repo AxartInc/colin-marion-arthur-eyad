683718577e0e56c36d7f5dc37cfe6db70c6ca414
atrantan <atrantan@gmail.com>
2018-05-12 19:00:14 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,28 +1,71 @@
 #include <Arduino.h>
 #include "include/timer.h"
 
-uint32_t state = 0;
+/// The class fsm_state is a type whose value is restricted to a 
+/// subset of values
+enum class fsm_state 
+{
+    s0, s1
+};
+
+// Declaration of a variable of type fsm_state
+fsm_state my_state = fsm_state::s0;
 
 void automate()
 {
-    static int toto = 0;
+    static int ms = 0;
 
-    if(toto == 999)
+    // Update the state only every 1000 function calls
+    if(ms == 999)
     {
-        toto = 0;
-        state = state ? LOW : HIGH;
+        ms = 0;
+
+        // Manage the state transitions from the state value
+        switch (my_state)
+        {
+            case fsm_state::s0:
+
+                my_state = fsm_state::s1;
+
+                break;
+
+            case fsm_state::s1:
+
+                my_state = fsm_state::s0;
+
+                break;
+        }
     }
     else
-        toto++;
+    {
+        ms++;
+    }
 }
 
 void setup()
 {
     pinMode(13,OUTPUT);
+
+    // Make the function automate() being called 
+    // every 1000 microseconds
     set_timer4_interrupt(1000,automate);
 }
 
 void loop()
 {
-    digitalWrite(13,state);
+    // Manage the outputs from the state value
+    switch (my_state)
+    {
+        case fsm_state::s0:
+
+            digitalWrite(13,LOW);
+
+            break;
+
+        case fsm_state::s1:
+
+            digitalWrite(13, HIGH);
+
+            break;
+    }
 }
