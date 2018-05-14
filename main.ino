d0b6438a9f0527293d80732aabba749f227e4f3e
AxartInc <axart@protonmail.com>
2018-05-14 18:07:17 +0200

    on a rassemblé la branche master et exemple dans partie_pratique

--- main.ino
+++ main.ino
@@ -13,6 +13,48 @@ int8_t TimeDisp[] = {0, 0, 0, 0};
 // Declaration of a TM1637 variable
 TM1637 tm1637(CLK, DIO);
 
+/// The class fsm_state is a type whose value is restricted to a
+/// subset of values
+enum class fsm_state
+{
+    s0,
+    s1
+};
+
+// Declaration of a variable of type fsm_state
+fsm_state my_state = fsm_state::s0;
+
+void automate()
+{
+    static int ms = 0;
+
+    // Update the state only every 1000 function calls
+    if (ms == 999)
+    {
+        ms = 0;
+
+        // Manage the state transitions from the state value
+        switch (my_state)
+        {
+        case fsm_state::s0:
+
+            my_state = fsm_state::s1;
+
+            break;
+
+        case fsm_state::s1:
+
+            my_state = fsm_state::s0;
+
+            break;
+        }
+    }
+    else
+    {
+        ms++;
+    }
+}
+
 void setup()
 {
     tm1637.set();
@@ -40,6 +82,21 @@ void loop()
         tm1637.point(POINT_OFF);
         display_point = 0;
     }
-
     tm1637.display(TimeDisp);
+
+    // Manage the outputs from the state value
+    switch (my_state)
+    {
+    case fsm_state::s0:
+
+        digitalWrite(13, LOW);
+
+        break;
+
+    case fsm_state::s1:
+
+        digitalWrite(13, HIGH);
+
+        break;
+    }
 }
